07882663e30c3a7a1c498a7dc98f234e6c0fe08b
Braulin Pires <braulinpires@hotmail.com>
2024-04-19 00:16:02 +0100

    added a new component using stories within stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default {
     //title is mandatory and should be unique:
-    title: 'Button',
+    title: 'Form/Button',
     component: Button, //our component imported
 }
 
@@ -11,3 +11,7 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+//creating a template:
+
+const Template = args => <Button {...args} />
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -3,11 +3,13 @@ import Input from "./Input";
 
 export default {
     //title is mandatory and should be unique:
-    title: 'Input',
+    title: 'Form/Input',
     component: Input, //our component imported
 }
 
-export const Primary = () => <Input variant='primary'/>
-export const Secondary = () => <Input variant='secondary'/>
-export const Success = () => <Input variant='success'/>
-export const Danger = () => <Input variant='danger'/>
+export const PrimaryInput = () => <Input variant='primary' placeholder="Type your name"/>
+export const SecondaryInput = () => <Input variant='secondary'/>
+export const SuccessInput = () => <Input variant='success'/>
+export const DangerInput = () => <Input variant='danger'/>
+
+//Primary.storyName = 'Small' to change the name displayed in storybook without change the import name
