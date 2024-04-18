9627e6446a910795da882270374fc1a9eefdb103
Braulin Pires <braulinpires@hotmail.com>
2024-04-19 00:32:27 +0100

    passing props using args in Button stories component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,16 +2,43 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-    //title is mandatory and should be unique:
-    title: 'Form/Button',
-    component: Button, //our component imported
-}
+  //title is mandatory and should be unique:
+  title: "Form/Button",
+  component: Button, //our component imported
+  //to pass all the component with the same args children:
+  args: {
+    children: 'Button'
+  }
+};
 
-export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Secondary</Button>
-export const Success = () => <Button variant='success'>Success</Button>
-export const Danger = () => <Button variant='danger'>Danger</Button>
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
 
 //creating a template:
 
-const Template = args => <Button {...args} />
+const Template = (args) => <Button {...args} />;
+
+// uma função que renderiza o componente <Button> com os props que forem passados para ela.
+export const PrimaryA = Template.bind({});
+
+//Estamos definindo os argumentos (props) específicos para o componente PrimaryA
+PrimaryA.args = {
+  variant: "primary",
+//   children: "Primary Args",
+};
+
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+//   children: "Secondary Args",
+};
+
+export const LongPrimaryA = Template.bind({});
+//you can extend the props from others stories, in this case from PrimaryA stories
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    // children: 'Long Primary'
+}
