9a5ad36e0d3c60753a21ca09706e82b876281eff
Braulin Pires <braulinpires@hotmail.com>
2024-04-19 10:38:39 +0100

    added theme on stories using  chakra but not working yet

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
+import {ThemeProvider, theme, CssReset, Box} from '@chakra-ui/react'
+import { addDecorator } from '@storybook/react'
+
+// addDecorator (story => (
+//   <ThemeProvider theme={theme}>
+//     <CssReset/>
+//     <Box m='4'>{story()}</Box>
+
+//   </ThemeProvider>
+// ))
 const preview = {
   parameters: {
     controls: {
@@ -8,6 +18,8 @@ const preview = {
       },
     },
   },
+  
 };
 
 export default preview;
+
---
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from '@chakra-ui/react'
+
+
+export default {
+    title: 'Chakra/Button',
+    comment: Button
+}
+
+export const Success = () => <Button variantColor='green'>Success</Button>
+
+export const Danger = () => <Button variantColor='red'>Danger</Button>
